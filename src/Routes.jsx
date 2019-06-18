3aafa6a6b07419c41b93b6119799618e498dab7a
Orest Bats <orest.bats@gmail.com>
2019-06-18 15:22:45 +0300

    Add sign up/in forms (#12)

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import MessageView from './components/Message/View/View';
 import MessageContainer from './components/Message/Container/Container';
+import SignUp from './components/Auth/SignUp/SignUp';
+import SignIn from './components/Auth/SignIn/SignIn';
 
 const Routes = () => (
   <Fragment>
@@ -10,6 +12,8 @@ const Routes = () => (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/mail/category" component={MessageContainer} />
       <Route exact path="/:id" component={MessageView} />
+      <Route exact path="/auth/signup" component={SignUp} />
+      <Route exact path="/auth/signin" component={SignIn} />
     </Switch>
   </Fragment>
 );
---
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import { Link } from 'react-router-dom';
+import { Button, TextField } from '@material-ui/core';
+import axios from '../../../api/axios';
+import { signUpSchema, initialValues } from './validation';
+import { errorToaster } from '../../UI/Toaster/Toaster';
+import './SignUp.css';
+import successIcon from './success.svg';
+
+const SignUp = ({ history }) => {
+  const [isRotate, setIsRotate] = useState(false);
+
+  const onSubmitClick = values => {
+    axios
+      .post('/auth/signup', values)
+      .then(() => {
+        setIsRotate(true);
+      })
+      .catch(() => {
+        errorToaster('Something went wrong. Try again later.');
+      });
+  };
+
+  const onOkButtonClick = () => {
+    history.push('/');
+  };
+
+  const SignUpView = props => {
+    const { values, handleChange, handleBlur, handleSubmit, errors, touched } = props;
+
+    return (
+      <div className="signup-container">
+        <div className={`signup-wrapper ${isRotate ? 'signup-wrapper--rotate' : ''}`}>
+          <Form className="signup-form" onSubmit={handleSubmit}>
+            <h2 className="signup-form__title">Create your Messanger Account</h2>
+            <TextField
+              fullWidth
+              error={!!errors.username && touched.username}
+              variant="outlined"
+              id="username-field"
+              label="Username"
+              name="username"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username || ''}
+              className="signup-form__input"
+            />
+            <TextField
+              fullWidth
+              error={!!errors.email && touched.email}
+              variant="outlined"
+              type="email"
+              id="email-field"
+              label="Email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email || ''}
+              className="signup-form__input"
+            />
+            <TextField
+              fullWidth
+              error={!!errors.password && touched.password}
+              variant="outlined"
+              type="password"
+              id="password-field"
+              label="Password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password || ''}
+              className="signup-form__input"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              className="signup-form__submit"
+            >
+              Sign Up
+            </Button>
+            <div className="signup-form__link">
+              <Link to={'/auth/signin'}>Sign in instead</Link>
+            </div>
+          </Form>
+
+          <div className="signup-success">
+            <header className="signup-success__header">
+              <img src={successIcon} alt="success" width="120" height="120" />
+              <h2>Registration completed successfully</h2>
+            </header>
+            <Button
+              type="button"
+              variant="contained"
+              color="primary"
+              className="signup-success__button"
+              onClick={onOkButtonClick}
+            >
+              OK
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={signUpSchema}
+      onSubmit={onSubmitClick}
+      render={SignUpView}
+    />
+  );
+};
+
+export default SignUp;
